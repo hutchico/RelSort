0b04c2e8e5b004b568b10cf85e492cd39036390e
hutchico <22420744+hutchico@users.noreply.github.com>
2021-02-25 12:46:00 -0800

    completed implementation of arrange/insert functions

--- Favorite.hpp
+++ Favorite.hpp
@@ -1,12 +1,12 @@
 #ifndef FAVORITE_HPP
 #define FAVORITE_HPP
 #include <string>
-#include <unordered_map>
 #include <map>
 #include <vector>
 #include <cstdlib>
 #include <ctime>
 #include <utility>
+#include <assert.h> //DEBUG
 
 using std::string;
 using std::cout;
@@ -63,14 +63,6 @@ void spit_data(vector<favorite> data){
     }
 };
 
-void enum_data(vector<favorite> input){
-    cout << "Contents:" << endl;
-    for(int i = 0; i < input.size(); i++){
-        cout << input[i].get_name() << endl;
-    }
-    cout << endl;
-};
-
 //DEBUG: print pairs
 void enum_pairs(vector<pair<string,string>> pairs){
     for(int i = 0; i < pairs.size(); i++){
@@ -79,7 +71,15 @@ void enum_pairs(vector<pair<string,string>> pairs){
     cout << endl;
 }
 
-void swap_members(vector<favorite> &data,map<string,int> &directory, int left, int right){
+void enum_data(vector<favorite> input){
+    cout << "Contents:" << endl;
+    for(int i = 0; i < input.size(); i++){
+        cout << input[i].get_name() << endl;
+    }
+    cout << endl;
+};
+
+void swap(vector<favorite> &data,map<string,int> &directory, int left, int right){
     favorite temp;
     if(left != right){
         cout << " swapping " << left << " and " << right << endl;
@@ -96,34 +96,59 @@ void swap_members(vector<favorite> &data,map<string,int> &directory, int left, i
         cout << "something went wrong, attempting to swap same position" << endl;
 };
 
-//'member' variable refers to the array position of the member being emplaced
-void emplace_member(vector<favorite> &data,map<string,int> &directory, int member){
+//insert member at position, shuffle dictionary references down one value
+void insert_item(vector<favorite> &data,map<string,int> &directory, string target, int loc, int mode){
 
+    assert(loc >= 0 && loc < data.size());
+
+    int targetLoc = directory[target];
+    favorite out = data[targetLoc];
+    int i;
+    
+    if(mode == 1){
+        i = loc; //placing above a value
+        data.erase(data.begin()+targetLoc);
+        data.insert(data.begin()+loc,out);
+    }
+    else{
+        i = targetLoc; //placing below a value
+        data.insert(data.begin()+loc,out);
+        data.erase(data.begin()+targetLoc);
+    }
+
+    if(loc == data.size()-1){ //perform a swap operation if we've inserted a value at the end of the vector
+        out = data[data.size()-1];
+        data[data.size()-1] = data[loc];
+        data[loc] = out;
+    }
+    for(; i < data.size(); i++){
+        directory[data[i].get_name()] = i;
+    }
 };
 
-int check_sort_compliant(vector<favorite> data, map<string,int> directory, int to_check){
-    //Verify that an array member at position to_check is positioned between all values greater than and less than itself
-    // If this isn't the case, return the array position of the offending member.
-    data[to_check].enum_relations();
+//Verify that an array member at position to_check is positioned between all values greater than and less than itself
+bool check_sort_compliant(vector<favorite> data, map<string,int> directory, int to_check){
+    //data[to_check].enum_relations();
     int refPos = directory[data[to_check].get_name()];
     for(int i = 0; i < data[to_check].get_highSize(); i++){
         int testRef = directory[data[to_check].read_higher(i)];
         if(refPos < testRef)
             continue;
         else
-            return testRef;
+            return false;
     }
     for(int i = 0; i < data[to_check].get_lowSize(); i++){
         int testRef = directory[data[to_check].read_lower(i)];
         if(refPos > testRef)
             continue;
         else
-            return testRef;
+            return false;
     }
-    return -1;
+    return true;
 };
 
-void record_compare(favorite &left, favorite &right){
+//ask user which member is greater, record result implictly within each member
+void compare(favorite &left, favorite &right){
     int choice;
     cout << "Left or right, 1 or 0:" << endl;
     cout << left.get_name() << " or " << right.get_name() << "?" << endl;
@@ -161,81 +186,70 @@ bool test_pair(pair<string,string> input, vector<pair<string,string>> db){
     return false;
 };
 
-//remove a given name from the tracking vector and return its position
-void remove_name(vector<string> &subset, string toRem){
-    //DEBUG
-    for(int i = 0; i < subset.size(); i++){
-        if(subset[i] == toRem){
-            subset.erase(subset.begin()+i);
-            break;
-        }
-    }
-};
-
-//follow name removal by removing references to that name
-void remove_pairs(vector<pair<int,int>> &tried, int value){
-    //NOTE: value refers to the array position of a value, not test case name!
-    
-    for(int i = 0; i <= tried.size(); i++){
-        if(tried[i].first == value || tried[i].second == value){
-            tried.erase(tried.begin()+i);
-            i = 0;
-        }
-    }
-}
-
 void survey(vector<favorite> &data,map<string,int> directory){
     srand((unsigned)time(0));
-    vector<string> subset;  //records members which have less than N relations
     vector<pair<string,string>> tried;
-    //map<string,int> times_compared;
     pair<string,string> compair;
-    for(int i = 0; i < data.size(); i++){
-        subset.push_back(data[i].get_name());
-        //times_compared[data[i].get_name()] = 0; //exception prevention
-    }
+    int maxRoll = data.size();
     while(!confirm_relation_min(data)){
-        int maxRoll = subset.size();
-        int left = rand() % maxRoll;    //roll two array positions
+        int left = rand() % maxRoll;    
         int right = rand() % maxRoll;
         if(right == left)
             continue; //Don't compare a member to itself
 
-        string leftName = subset[left];
-        string rightName = subset[right];
-        compair.first = leftName;
-        compair.second = rightName;
+        compair.first = data[left].get_name();
+        compair.second = data[right].get_name();
         if(test_pair(compair,tried)) //this value is a repeat, start over and hopefully get a different set.
             continue; 
         tried.push_back(compair);
 
-        left = directory[leftName];
-        right = directory[rightName];
-        record_compare(data[left],data[right]); //perform comparison, modify relations
-
-        /*
-        times_compared[leftName] = times_compared[leftName] + 1;
-        times_compared[rightName] = times_compared[rightName] + 1;
-        */
-        enum_pairs(tried);
-        /*
-        if(data[left].get_hierarchy_size() >= NUM_SURVEYS){
-            cout << "removing left:" << leftName << endl; //DEBUG: verify branch
-            remove_name(subset,data[left].get_name());   
-        }
-        if(data[right].get_hierarchy_size() >= NUM_SURVEYS){
-            cout << "removing right:" << rightName << endl; //DEBUG: verify branch
-            remove_name(subset,data[right].get_name());
-        }
-        */
+        compare(data[left],data[right]); //perform comparison, modify relations
     }
-
 
 };
 
+//determine the array position directly below this instance
+//as in, lowest array position this instance can be above
+int find_greatest_array_val(favorite input, map<string,int> directory){
+    int greatest = INT32_MAX; //arbitrary large number
+    for(int i = 0; i < input.get_lowSize(); i++){
+        int expanded = directory[input.read_lower(i)];
+        if(expanded < greatest)
+            greatest = expanded;
+    }
+    assert(greatest < INT32_MAX);
+    return greatest;
+}
+
+//determine the array position directly above this instance
+//as in, highest array position this instance can be below
+int find_lowest_array_val(favorite input, map<string,int> directory){
+    int lowest = 0;
+    for(int i = 0; i < input.get_highSize(); i++){
+        int expanded = directory[input.read_higher(i)];
+        if(expanded > lowest)
+            lowest = expanded;
+    }
+    assert(lowest != 0);
+    return lowest;
+}
+
 void arrange(vector<favorite> &data,map<string,int> &directory){
     //Start from the bottom of the list, move entry to just above its highest entry
-    
+    int current_ref = data.size();
+    while(current_ref > 0){
+        if(!check_sort_compliant(data,directory,current_ref)){
+            if(data[current_ref].get_lowSize() > 0) //Attempt to place instance above its highest low value
+                insert_item(data,directory,data[current_ref].get_name(),find_greatest_array_val(data[current_ref],directory), 1);
+            else{ //if this isn't possible, just move it to the bottom of the array and start over.
+                int pos = find_lowest_array_val(data[current_ref],directory);
+                insert_item(data,directory,data[current_ref].get_name(),pos,2);
+                current_ref = data.size();
+            }
+        }
+        else
+            current_ref--;
+    }
 };
 
 void RelSort(vector<favorite> &data){
